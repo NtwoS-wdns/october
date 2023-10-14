5d70f54541aea5d02b9cf1984525c6c4b5c5691b
NtwoS-wdns <68218290+NtwoS-wdns@users.noreply.github.com>
2023-10-14 09:24:18 -0400

    Delete lasttest directory

--- lasttest/STBU_Core.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "STBU_Core.h"
-
--- lasttest/lasttest.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "lasttest.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, lasttest, "lasttest" );
- 
