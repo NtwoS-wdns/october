26efecfa96783f86d77b337cc1057fb67c31ca65
NtwoS-wdns <68218290+NtwoS-wdns@users.noreply.github.com>
2023-10-14 09:24:10 -0400

    Delete Source directory

--- Source/SolitudeSurvival/Private/CharacterComponents/SLS_Controller.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "CharacterComponents/SLS_Controller.h"
-#include "Net/UnrealNetwork.h"
-
-ASLS_Controller::ASLS_Controller() {
-
-}
